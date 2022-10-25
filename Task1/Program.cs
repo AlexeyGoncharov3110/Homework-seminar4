a33e37c5b241bdd1aaaa5d5e18bef064bfd627ca
Alexey <slow.net@mail.ru>
2022-10-25 18:19:18 +0300

    Решение первой задачи ДЗ семинара 4

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите два числа");
+
+bool isParsedA = int.TryParse(Console.ReadLine(), out int a);
+bool isParsedB = int.TryParse(Console.ReadLine(), out int b);
+
+if (!isParsedA || !isParsedB || b <= 0)
+{
+    Console.WriteLine("Ошибка! Не правильно введены данные");
+    return;
+}
+int numberDegree = NumberDegree(a, b);
+
+int NumberDegree(int digit, int degree)
+{
+    int result = digit;
+    for (int i = 1; i < degree; i++)
+    {
+        result *= digit;
+    }
+return result;
+}
+
+Console.WriteLine($"Число {a} в {b} степени равно {numberDegree}");
