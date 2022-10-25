dd6d4f6793221ee78567a3d4a1b4197c6ad94183
Alexey <slow.net@mail.ru>
2022-10-25 18:21:59 +0300

    Решение второй задачи ДЗ семинара 4

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число");
+
+bool isParsedA = int.TryParse(Console.ReadLine(), out int a);
+if (!isParsedA || a <= 0)
+{
+    Console.WriteLine("Вы ввели не корректные данные");
+}
+
+int amountOfNumders = AmountOfNumders(a);
+int AmountOfNumders(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+    return result;
+}
+Console.WriteLine($"Сумма цифр в числе = {amountOfNumders}");
